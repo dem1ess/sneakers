7ab8834ff34872b8ceb15b05be3510c2441dc08c
Харченко Станислав <49039456+dem1ess@users.noreply.github.com>
2021-06-28 16:32:05 +0300

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+function App() {
+  return (
+    <div className="wrapper">
+        <header>
+            <div>
+            <svg />
+            <div>
+              <h3>React Sneakers</h3>
+              <p>Магазин лучших кроссовок</p>
+            </div>
+              <div>
+              <ul>
+                <li><svg/><span>6400 UAN</span></li>
+                <li><svg/></li>
+              </ul>
+              </div>
+            </div>
+        </header>
+    </div>
+  );
+}
+
+export default App;
